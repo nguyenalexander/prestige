2aaa6153829b7a89f1615a8b537a29d4e1cf8b30
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-07-08 15:23:10 -0700

    fixed logo add navbar

--- js/agency.js
+++ js/agency.js
@@ -6,14 +6,14 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Home', 'Services', 'About'],
+    showActiveTooltip: true,
+    // scrollOverflow: true,
+    fitToSection: false
+  });
 
 });
 
