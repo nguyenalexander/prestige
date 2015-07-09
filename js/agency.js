79b7ce1dd4daa879d1c3d5479335064d3d175893
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-07-08 17:01:47 -0700

    modals

--- js/agency.js
+++ js/agency.js
@@ -15,6 +15,24 @@ $(function() {
     fitToSection: false
   });
 
+  // var options = [
+  //   {selector: '.class', offset: 200, callback: 'globalFunction()' },
+  //   {selector: '.other-class', offset: 200, callback: 'globalFunction()' },
+  // ];
+
+  // Materialize.scrollFire(options);
+
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { console.log('Ready'); }, // Callback for Modal open
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+    }
+  );
+
 });
 
 
